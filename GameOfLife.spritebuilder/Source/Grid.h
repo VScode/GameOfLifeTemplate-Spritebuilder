2a24c71bdb6d4ca7f66a417c8555a807c12fcce4
Manuel <elvictorfiee@gmail.com>
2014-07-24 18:38:50 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.h
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -13,5 +13,10 @@
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
 
+- (void)evolveStep;
+- (void)countNeighbors;
+- (BOOL)isIndexValidForX:(int)x andY:(int)y;
+- (void)updateCreatures;
+
 
 @end
